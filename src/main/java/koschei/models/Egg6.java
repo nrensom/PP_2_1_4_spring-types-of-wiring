b2d89bef58c5a098c66ecd63b61487161097740e
Anastasia Rensom <nrensom666@gmail.com>
2022-06-01 22:05:07 +0300

    //

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
 public class Egg6 {
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
+
+    private Needle7 needle;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
